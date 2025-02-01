19aec0ebbd21cac3f9504cc665997da77391f9cd
kazigoni <kazimdgoni@gmail.com>
2025-02-01 23:58:03 +0600

    user router deleted. it was created to practice routing

--- api/routes/user.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-user_router = APIRouter(prefix="/user", tags=["user"])
-
-
-@user_router.get("/")
-def all_users():
-    return "No Users yet"
--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from api.routes.todo import todo_router
-from api.routes.user import user_router
 
 
 app = FastAPI()
 app.include_router(todo_router)
-app.include_router(user_router)
 
 
 @app.get("/")
